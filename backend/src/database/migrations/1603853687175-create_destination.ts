190d7febed37fbd4ff043a0b14021d8a690ae56b
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-10-29 17:53:37 -0300

    migration e database sqlite adicionado

---
+++ backend/src/database/migrations/1603853687175-create_destination.ts
@@ -0,0 +1,60 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createDestination1603853687175 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'destinations',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'integer',
+                    unsigned: true,
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: "increment",
+                },
+                {
+                    name: 'name',
+                    type: 'varchar',
+                },
+                {
+                    name: 'phone',
+                    type: 'number',
+                },
+                {
+                    name: 'email',
+                    type: 'varchar',
+                },
+                {
+                    name: 'opening_hours',
+                    type: 'varchar',
+                },
+                {
+                    name: 'adress',
+                    type: 'varchar',      
+                },
+                {
+                    name: 'cep',
+                    type: 'number',  
+                },
+                {
+                    name: 'latitude',
+                    type: 'decimal',
+                    scale: 10,
+                    precision: 2
+                },
+                {
+                    name: 'longitude',
+                    type: 'decimal',
+                    scale: 10,
+                    precision: 2
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
