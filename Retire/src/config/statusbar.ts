9382132f4c1ddd7f1e1df776505b8421a5ca7415
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-21 22:53:22 -0300

    style: add statusbar config

---
+++ Retire/src/config/statusbar.ts
@@ -0,0 +1,7 @@
+import { StatusBar } from 'react-native';
+
+StatusBar.setBackgroundColor('transparent');
+StatusBar.setBarStyle('dark-content');
+StatusBar.setTranslucent(true);
+
+export default StatusBar;
