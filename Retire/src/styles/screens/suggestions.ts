5f01e0d6588a9762b5499ecf3f330078b0677d7b
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-24 13:17:50 -0300

    feat: implements layout and styles for SuggestionReview screen

--- Retire/src/styles/screens/suggestions.ts
+++ Retire/src/styles/screens/suggestions.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import MapboxGL from '../../services/mapbox';
 import colors from '../colors';
 
 export const MapInputBlock = styled.View`
@@ -73,4 +74,81 @@ export const AddImage = styled.TouchableOpacity`
 
 export const AddImageText = styled.Text`
   color: ${colors.main_green};
+`;
+
+export const ReviewScroll = styled.ScrollView`
+  margin-top: 78px;
+`;
+
+export const AlignBlock = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 35px;
+`;
+
+export const ReviewTitle = styled.Text`
+  font-size: 20px;
+  color: ${colors.main_green};
+  font-weight: bold;
+  margin-left: 7px;
+`;
+
+export const ReviewText = styled.Text`
+  color: ${colors.text_grey};
+  line-height: 28px;
+`;
+
+export const ReviewBlock = styled.View`
+  width: 361px;
+`;
+
+export const ReviewTextBlock = styled.View`
+  margin-top: 24px;
+`;
+
+export const ReviewMap = styled(MapboxGL.MapView)`
+  height: 205px;
+  margin-top: 9px;
+  border-color: ${colors.main_grey};
+  border-width: 1px;
+`;
+
+export const ReviewImage = styled.Image`
+  width: 361px;
+  height: 211px;
+  margin: 21px 0;
+
+  border-radius: 15px;
+  border-width: 1px;
+  border-color: ${colors.main_grey};
+`;
+
+export const ReviewImageBlock = styled.View`
+  flex-direction: row;
+`;
+
+export const ReviewImageMini = styled(ReviewImage)`
+  width: 55px;
+  height: 55px;
+  margin-right: 12px;
+`;
+
+export const ReviewFinish = styled.TouchableOpacity`
+  width: 259px;
+  height: 57px;
+  border-radius: 20px;
+
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${colors.second_green};
+  elevation: 3;
+
+  margin: 24px 0;
+`;
+
+export const ReviewFinishText = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #fff;
 `;
