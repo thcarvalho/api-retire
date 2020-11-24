40ebed19d6d152612eadebfd3da0daf6a4c820cc
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-24 13:44:45 -0300

    folder migration

--- Retire/src/services/mapbox.ts
+++
@@ -1,8 +0,0 @@
-import MapboxGL from '@react-native-mapbox-gl/maps';
-import { REACT_APP_MAPBOX_TOKEN } from '@env';
-
-MapboxGL.setAccessToken(REACT_APP_MAPBOX_TOKEN);
-
-MapboxGL.setConnected(true);
-
-export default MapboxGL;
--- Retire/src/styles/screens/destination-list.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/native';
-
-export const DestinationScroll = styled.ScrollView`
-  margin-top: 78px;
-  padding: 0 12px;
-`;
